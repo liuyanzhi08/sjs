8583c488f6c366c4f73aff47875bb21e60b07f5c
lyz <702368372@qq.com>
2015-04-09 15:18:58 +0800

    tab->space

--- js/helper.js
+++ js/helper.js
@@ -1,12 +1,12 @@
 define(function() {
-	'use strict'
-	return {
-		repeat: function(str, times) {
-			var ret = '';
-			for (var i = 0; i < times; i++) {
-				ret += str;
-			}
-			return ret;
-		}
-	}
+  'use strict'
+  return {
+    repeat: function(str, times) {
+      var ret = '';
+      for (var i = 0; i < times; i++) {
+        ret += str;
+      }
+      return ret;
+    }
+  }
 })
